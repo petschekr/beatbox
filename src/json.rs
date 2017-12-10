865318db01635e4a9b3b43e65e7a4ddc840cdf45
Ryan Petschek <petschekr@gmail.com>
2017-12-09 19:19:46 -0500

    Implement getting library and stream URLs

--- src/json.rs
+++ src/json.rs
@@ -1,34 +1,130 @@
-#[allow(non_snake_case)]
-#[derive(Deserialize)]
-pub struct SettingsResponse {
-    pub settings: Settings,
-}
+extern crate serde;
+extern crate serde_json;
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-pub struct Settings {
-	pub uploadDevice: Vec<Device>,
-	pub entitlementInfo: EntitlementInfo,
-	pub subscriptionNewsletter: bool,
-}
+pub mod settings {
+	#[allow(non_snake_case)]
+	#[derive(Deserialize)]
+	pub struct Response {
+		pub settings: Settings,
+	}
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-pub struct Device {
-	pub id: String,
-	pub lastEventTimeMillis: u64,
-	pub lastAccessedTimeMillis: u64,
-	pub carrier: Option<String>,
-	pub manufacturer: Option<String>,
-	pub model: Option<String>,
-	pub deviceType: u8,
-	pub lastAccessedFormatted: String,
-}
+	#[allow(non_snake_case)]
+	#[derive(Deserialize, Debug)]
+	pub struct Settings {
+		pub uploadDevice: Vec<Device>,
+		pub entitlementInfo: EntitlementInfo,
+		pub subscriptionNewsletter: bool,
+	}
 
-#[allow(non_snake_case)]
-#[derive(Deserialize, Debug)]
-pub struct EntitlementInfo {
-	pub isTrial: bool,
-	pub isCanceled: bool,
-	pub expirationMillis: u64,
+	#[allow(non_snake_case)]
+	#[derive(Deserialize, Debug)]
+	pub struct Device {
+		pub id: String,
+		pub lastEventTimeMillis: u64,
+		pub lastAccessedTimeMillis: u64,
+		pub carrier: Option<String>,
+		pub manufacturer: Option<String>,
+		pub model: Option<String>,
+		pub deviceType: u8,
+		pub lastAccessedFormatted: String,
+	}
+
+	#[allow(non_snake_case)]
+	#[derive(Deserialize, Debug)]
+	pub struct EntitlementInfo {
+		pub isTrial: bool,
+		pub isCanceled: bool,
+		pub expirationMillis: u64,
+	}
+}
+pub mod library {
+	use super::serde::{Deserialize, Deserializer};
+	use super::serde_json::{self, Value, Map};
+
+	#[derive(Serialize)]
+	pub struct Request {
+		#[serde(rename = "max-results")]
+		pub limit: u32,
+		#[serde(rename = "start-token")]
+		pub next_page_token: String
+	}
+
+	#[derive(Deserialize, Debug)]
+	pub struct Response {
+		pub kind: String,
+		#[serde(rename = "nextPageToken")]
+		pub next_page_token: Option<String>,
+		#[serde(rename = "data")]
+		#[serde(deserialize_with = "extract_data")]
+		pub tracks: Vec<Track>,
+	}
+	fn extract_data<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<Track>, D::Error> {
+		let n: Map<String, Value> = Deserialize::deserialize(d)?;
+		let items = n.get("items").unwrap().clone();
+		Ok(serde_json::from_value(items).unwrap())
+	}
+	impl Response {
+		pub fn next_request(&self, tracks: &Vec<Track>) -> Option<Request> {
+			match self.next_page_token {
+				None => None,
+				Some(ref token) => Some(Request {
+					limit: tracks.len() as u32,
+					next_page_token: token.clone(),
+				})
+			}
+		}
+	}
+	
+	#[derive(Deserialize, Debug)]
+	#[serde(rename_all = "camelCase")]
+	pub struct Track {
+		pub kind: String,
+		pub deleted: bool,
+
+		pub id: String,
+		pub album_id: Option<String>,
+		pub artist_id: Option<Vec<String>>,
+		pub client_id: Option<String>,
+		pub store_id: Option<String>,
+		
+		#[serde(deserialize_with = "string_to_u64")]
+		pub creation_timestamp: u64,
+		#[serde(deserialize_with = "string_to_u64")]
+		pub last_modified_timestamp: u64,
+		#[serde(deserialize_with = "string_to_u64")]
+		pub recent_timestamp: u64,
+		
+		pub title: String,
+		pub artist: String,
+		pub album: String,
+		pub album_artist: String,
+		pub composer: Option<String>,
+		pub year: Option<u16>,
+		pub genre: Option<String>,
+		pub track_number: u16,
+		pub disc_number: u16,
+
+		#[serde(default)]
+		#[serde(rename = "artistArtRef")]
+		#[serde(deserialize_with = "extract_art")]
+		pub artist_art_url: Option<String>,
+		#[serde(default)]
+		#[serde(rename = "albumArtRef")]
+		#[serde(deserialize_with = "extract_art")]
+		pub album_art_url: Option<String>,
+		
+		#[serde(deserialize_with = "string_to_u64")]
+		pub duration_millis: u64,
+		#[serde(deserialize_with = "string_to_u64")]
+		pub estimated_size: u64,
+	}
+	fn string_to_u64<'de, D: Deserializer<'de>>(d: D) -> Result<u64, D::Error> {
+		let n: String = Deserialize::deserialize(d)?;
+		Ok(n.parse().unwrap())
+	}
+	fn extract_art<'de, D: Deserializer<'de>>(d: D) -> Result<Option<String>, D::Error> {
+		let n: Value = Deserialize::deserialize(d)?;
+		let url = n[0]["url"].clone();
+		Ok(serde_json::from_value(url).unwrap())
+	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate hyper;
 
 extern crate reqwest;
 extern crate ring;
@@ -7,25 +9,26 @@ extern crate openssl;
 extern crate base64;
 
 use std::collections::HashMap;
-use std::fmt::Write;
-use reqwest::header;
+use reqwest::{header, StatusCode};
 use ring::digest;
 use ring::rand::SecureRandom;
 use openssl::rsa::Rsa;
 use openssl::bn::BigNum;
 
 mod json;
+use json::{settings, library};
 
-const SIGNING_KEY: &'static str = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==";
+const LOGIN_SIGNING_KEY: &'static str = "AAAAgMom/1a/v0lblO2Ubrt60J2gcuXSljGFQXgcyZWveWLEwo6prwgi3iJIZdodyhKZQrNWp5nKJ3srRXcUW+F1BD3baEVGcmEgqaLZUNBjm057pKRI16kB0YppeGx5qIQ5QjKzsR8ETQbKLNWgRY0QRNVz34kMJR3P/LgHax/6rmf5AAAAAwEAAQ==";
+const STREAM_SIGNING_KEY: &'static str = "MzRlZTc5ODMtNWVlNi00MTQ3LWFhODYtNDQzZWEwNjJhYmY3NzQ0OTNkNmEtMmExNS00M2ZlLWFhY2UtZTc4NTY2OTI3NTg1Cg==";
 
 const BASE_URL: &'static str = "https://www.googleapis.com/sj/v1.11";
 const WEB_URL: &'static str = "https://play.google.com/music";
 const MOBILE_URL: &'static str = "https://android.clients.google.com/music";
-const ACCOUNT_URL: &'static str = "https://www.google.com/accounts";
 const AUTH_URL: &'static str = "https://android.clients.google.com/auth";
 
 pub struct Instance {
 	client: reqwest::Client,
+	rng: ring::rand::SystemRandom,
 	token: Option<String>,
 	device_id: Option<String>,
 }
@@ -40,15 +43,44 @@ pub struct TokenDetails {
 	token: String,
 }
 
+pub enum LibraryOptions {
+	Default,
+	Limit(u32),
+	NextPageToken(String),
+	LimitAndNextPageToken(u32, String)
+}
+
+#[derive(Debug)]
+pub enum Error {
+	Network(reqwest::Error),
+	User(&'static str),
+}
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Error {
+        Error::Network(err)
+    }
+}
+impl From<&'static str> for Error {
+	fn from(err: &'static str) -> Error {
+		Error::User(err)
+	}
+}
+
+header! { (XDeviceID, "X-Device-ID") => [String] }
+
 impl Instance {
 	fn new() -> Instance {
 		Instance {
-			client: reqwest::Client::new(),
+			client: reqwest::Client::builder()
+				.redirect(reqwest::RedirectPolicy::none())
+				.build().unwrap(),
+			rng: ring::rand::SystemRandom::new(),
 			token: None,
 			device_id: None,
 		}
 	}
 
+	/// Creates a new instance using a username and password
 	pub fn from_login(details: LoginDetails) -> Result<Instance, reqwest::Error> {
 		let mut instance = Instance::new();
 		
@@ -61,6 +93,7 @@ impl Instance {
 		Ok(instance)
 	}
 
+	/// Creates a new instance using a master token
 	pub fn from_token(details: TokenDetails) -> Result<Instance, reqwest::Error> {
 		let mut instance = Instance::new();
 
@@ -99,7 +132,6 @@ impl Instance {
 				3 => &device.id,
 				_ => continue
 			};
-			println!("Using device ID: {}", id);
 			self.device_id = Some(id.to_string());
 			break;
 		}
@@ -107,19 +139,16 @@ impl Instance {
 		Ok(())
 	}
 
-	pub fn generate_token<T: Into<String>>(details: LoginDetails, android_id: Option<T>) -> Result<TokenDetails, reqwest::Error> {
+	/// Generates a token from a username and password that can be used later to initialize a new instance
+	pub fn generate_token(details: LoginDetails, android_id: Option<&str>) -> Result<TokenDetails, reqwest::Error> {
 		let password = encrypt_login(details.email, details.password);
 		let android_id: String = match android_id {
-			Some(id) => id.into(),
+			Some(id) => id.to_string(),
 			None => {
 				let generator = ring::rand::SystemRandom::new();
 				let mut id: [u8; 8] = [0; 8];
 				generator.fill(&mut id).unwrap();
-				let mut hex_id = String::new();
-				for byte in id.iter() {
-					write!(&mut hex_id, "{:x}", byte).expect("Unable to write");
-				}
-				hex_id
+				id.to_hex()
 			}
 		};
 
@@ -147,23 +176,108 @@ impl Instance {
 		Ok(TokenDetails { token, android_id })
 	}
 
-	fn get_auth_header(&self) -> reqwest::header::Authorization<std::string::String> {
+	fn get_auth_header(&self) -> reqwest::header::Authorization<String> {
 		let token = self.token.as_ref().expect("You must call init() before accessing the API");
 		header::Authorization(format!("GoogleLogin auth={}", token))
 	}
 
-	pub fn get_settings(&mut self) -> Result<json::Settings, reqwest::Error> {
+	/// Returns settings and device ids authorized for account
+	pub fn get_settings(&mut self) -> Result<settings::Settings, reqwest::Error> {
 		let mut body = HashMap::new();
 		body.insert("sessionId", "");
 
 		let url = format!("{}/services/fetchsettings?u=0", WEB_URL);
-		let response: json::SettingsResponse = self.client
+		let response: settings::Response = self.client
 			.post(&url)
 			.header(self.get_auth_header())
 			.json(&body)
 			.send()?
 			.json()?;
 		Ok(response.settings)
+	}
+
+	/// Returns a list of all tracks in the user's library
+	/// 
+	/// Defaults to a limit of 1000 tracks
+	pub fn get_library(&self, options: LibraryOptions) -> Result<library::Response, Error> {
+		let mut request = library::Request {
+			limit: 1000,
+			next_page_token: String::new(),
+		};
+		match options {
+			LibraryOptions::Default => {},
+			LibraryOptions::Limit(limit) => request.limit = limit,
+			LibraryOptions::NextPageToken(token) => request.next_page_token = token,
+			LibraryOptions::LimitAndNextPageToken(limit, token) => {
+				request.limit = limit;
+				request.next_page_token = token;
+			}
+		}
+
+		let url = format!("{}/trackfeed", BASE_URL);
+		let response: library::Response = self.client
+			.post(&url)
+			.header(self.get_auth_header())
+			.json(&request)
+			.send()?
+			.json().unwrap();
+
+		Ok(response)
+	}
+
+	/// Returns a track's stream URL
+	pub fn get_stream_url(&self, track: &library::Track) -> Result<String, Error> {
+		if self.device_id.is_none() {
+			Err("Unable to find a usable device on your account, access from a mobile device and try again")?;
+		}
+		let key = base64::decode(STREAM_SIGNING_KEY).unwrap();
+		let key = ring::hmac::SigningKey::new(&digest::SHA1, &key);
+
+		let mut salt = [0u8; 13];
+		self.rng.fill(&mut salt).unwrap();
+		let salt: String = salt.to_hex();
+
+		let message = track.id.clone() + &salt;
+		let signature = base64::encode_config(
+			ring::hmac::sign(&key, message.as_bytes()).as_ref(),
+			base64::URL_SAFE_NO_PAD
+		);
+
+		let mut query = HashMap::new();
+		query.insert("u", "0");
+		query.insert("net", "wifi");
+		query.insert("pt", "e");
+		query.insert("targetkbps", "8310");
+		query.insert("slt", &salt);
+		query.insert("sig", &signature);
+		if track.id.chars().next().unwrap() == 'T' {
+			query.insert("mjck", &track.id);
+		}
+		else {
+			query.insert("songid", &track.id);
+		}
+
+		let url = reqwest::Url::parse_with_params(&format!("{}/mplay", MOBILE_URL), query).unwrap().into_string();
+		let response = self.client
+			.get(&url)
+			.header(self.get_auth_header())
+			.header(XDeviceID(self.device_id.as_ref().cloned().unwrap()))
+			.send()?;
+
+		match response.status() {
+			StatusCode::Found => {
+				let url = response.headers().get::<header::Location>().unwrap();
+				Ok(url.to_string())
+			},
+			_ => {
+				if let Err(err) = response.error_for_status() {
+					Err(err)?
+				}
+				else {
+					Err("Couldn't get stream URL")?
+				}
+			}
+		}
 	}
 }
 
@@ -184,7 +298,7 @@ fn encrypt_login(email: &str, password: &str) -> String {
 
 	// The components of Google's public key
 
-	let key = base64::decode(SIGNING_KEY).unwrap();
+	let key = base64::decode(LOGIN_SIGNING_KEY).unwrap();
 	let digest = digest::digest(&digest::SHA1, &key);
 
 	let modulus_length = bytes_to_u32(&key[0..4]) as usize;
@@ -214,18 +328,38 @@ fn parse_key_values(body: &str) -> HashMap<&str, &str> {
 	parsed
 }
 
+const HEX_CHARS: &'static [u8] = b"0123456789abcdef";
+trait ToHex {
+    fn to_hex(&self) -> String;
+}
+impl ToHex for [u8] {
+    fn to_hex(&self) -> String {
+        let mut v = Vec::with_capacity(self.len() * 2);
+        for &byte in self {
+            v.push(HEX_CHARS[(byte >> 4) as usize]);
+            v.push(HEX_CHARS[(byte & 0xf) as usize]);
+        }
+
+        unsafe {
+            String::from_utf8_unchecked(v)
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
 	fn get_login_details<'a>() -> super::LoginDetails<'a> {
 		super::LoginDetails {
 			email: "petschekr@gmail.com",
-			password:include_str!("password.txt"),
+			password: include_str!("password.txt"),
 		}
 	}
 	#[test]
 	fn init() {
 		let instance = super::Instance::from_login(get_login_details()).unwrap();
+		let tracks = instance.get_library(super::LibraryOptions::Default).unwrap().tracks;
+		let url = instance.get_stream_url(&tracks[0]).unwrap();
+		println!("Got stream URL for {} by {}: {}", tracks[0].title, tracks[0].artist, url);
 	}
 	#[test]
 	fn generate_token() {
